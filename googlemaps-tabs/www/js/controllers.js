19ee7c91875a2c2433c75a9923ea4846b1c0b29b
sgudupat <raviranjan3930@gmail.com>
2016-12-05 13:04:35 +0530

    I5

---
+++ googlemaps-tabs/www/js/controllers.js
@@ -0,0 +1,42 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope, NgMap) {
+
+    NgMap.getMap().then(function(map) {
+//        console.log(map.getCenter());
+//        console.log('markers', map.markers);
+//        console.log('shapes', map.shapes);
+    });
+    
+    
+    
+})
+
+
+
+.controller('ChatsCtrl', function($scope, Chats) {
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
+
+    $scope.chats = Chats.all();
+    $scope.remove = function(chat) {
+        Chats.remove(chat);
+    };
+    
+      
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+    $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+    $scope.settings = {
+        enableFriends: true
+    };
+});
